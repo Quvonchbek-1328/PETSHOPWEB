79e383339e0752f379c67956a6724437222c31b4
Jahongir <95927694+JFaithTrust@users.noreply.github.com>
2024-07-16 21:37:17 +0500

    Update axios.ts

--- src/store/axios.ts
+++ src/store/axios.ts
@@ -5,7 +5,7 @@ const https = require('https');
 // const BASE_URL = "http://172.16.14.23:9090";
 
 // local url
-const BASE_URL = "http://45.130.148.122:27";
+const BASE_URL = "https://45.130.148.122:27";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
