7fba66580e1ce5d911daf58b1825d8c9080aa306
Jahongir <95927694+JFaithTrust@users.noreply.github.com>
2024-07-16 17:00:20 +0500

    Update axios.ts

--- src/store/axios.ts
+++ src/store/axios.ts
@@ -4,7 +4,7 @@ import axios, { AxiosInstance } from "axios";
 // const BASE_URL = "http://172.16.14.23:9090";
 
 // local url
-const BASE_URL = "https://visually-informed-manatee.ngrok-free.app";
+const BASE_URL = "http://45.130.148.122:27";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
